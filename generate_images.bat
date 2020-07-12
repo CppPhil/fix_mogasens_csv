e3ff4751786e79cda9681623c3da46205547cda5
CppPhil <CppPhil@users.noreply.github.com>
2020-07-12 11:44:04 +0200

    Handling of errorlevel in batch scripts

--- generate_images.bat
+++ generate_images.bat
@@ -7,7 +7,10 @@ set goto_ruby="%dir%\external\goto-ruby\goto-ruby.bat"
 cd %dir%
 
 call %goto_ruby% "%dir%\ruby\generate_images.rb" %*
-IF %ERRORLEVEL% NEQ 0 cd %prev_dir% & EXIT /B %ERRORLEVEL%
+if errorlevel 1 (
+  cd %prev_dir%
+  exit /B 1
+)
 
 cd %prev_dir%
 exit /B 0
