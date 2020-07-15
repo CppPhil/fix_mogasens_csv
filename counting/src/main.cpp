1b1ea0af1d8f476149ac75646513e50e3529af0d
CppPhil <CppPhil@users.noreply.github.com>
2020-07-15 11:47:18 +0200

    Adjust command line interface

--- counting/src/main.cpp
+++ counting/src/main.cpp
@@ -10,61 +10,63 @@
 
 int main(int argc, char* argv[])
 {
-  constexpr int expectedArgumentCount{2};
-  constexpr int filePathIndex{1};
+  constexpr int minimumArgumentCount{2};
 
-  if (argc != expectedArgumentCount) {
+  if (argc < minimumArgumentCount) {
     fmt::print(
       stderr,
-      "{}: {} command line arguments given, but {} were expected, exiting.\n",
+      "{}: {} command line arguments given, but at least {} were expected, "
+      "exiting.\n",
       argv[0],
       argc,
-      expectedArgumentCount);
+      minimumArgumentCount);
     return EXIT_FAILURE;
   }
 
-  const pl::string_view filePath{argv[filePathIndex]};
-  const cl::Expected<std::vector<std::vector<std::string>>> readFileResult{
-    cl::readCsvFile(filePath)};
+  for (int i{1}; i < argc; ++i) {
+    const pl::string_view                                     filePath{argv[i]};
+    const cl::Expected<std::vector<std::vector<std::string>>> readFileResult{
+      cl::readCsvFile(filePath)};
 
-  if (!readFileResult.has_value()) {
+    if (!readFileResult.has_value()) {
+      fmt::print(
+        stderr,
+        "{}: couldn't read \"{}\", error: \"{}\".\n",
+        argv[0],
+        filePath,
+        readFileResult.error());
+      return EXIT_FAILURE;
+    }
+
+    const cl::Expected<cl::DataSet> expectedDataSet{
+      cl::DataSet::create(filePath.to_string(), readFileResult.value())};
+
+    if (!expectedDataSet.has_value()) {
+      fmt::print(
+        stderr,
+        "{}: couldn't create data set from \"{}\", error: \"{}\".\n",
+        argv[0],
+        filePath,
+        expectedDataSet.error());
+      return EXIT_FAILURE;
+    }
+
+    fmt::print("{}: processing file \"{}\"\n", argv[0], filePath);
+
+    const cl::DataSet& dataSet{expectedDataSet.value()};
+
+    constexpr long double            accelerometerThreshold{1.99L};
+    constexpr long double            gyroscopeThreshold{1999.99L};
+    const std::vector<cl::DataPoint> aboveThresholdDataPoints{
+      ctg::aboveThreshold(dataSet, accelerometerThreshold, gyroscopeThreshold)};
+
     fmt::print(
-      stderr,
-      "{}: couldn't read \"{}\", error: \"{}\".\n",
-      argv[0],
-      filePath,
-      readFileResult.error());
-    return EXIT_FAILURE;
-  }
+      "{} data points above / below threshold were found.\n",
+      aboveThresholdDataPoints.size());
 
-  const cl::Expected<cl::DataSet> expectedDataSet{
-    cl::DataSet::create(filePath.to_string(), readFileResult.value())};
-
-  if (!expectedDataSet.has_value()) {
-    fmt::print(
-      stderr,
-      "{}: couldn't create data set from \"{}\", error: \"{}\".\n",
-      argv[0],
-      filePath,
-      expectedDataSet.error());
-    return EXIT_FAILURE;
-  }
-
-  fmt::print("{}: processing file \"{}\"\n", argv[0], filePath);
-
-  const cl::DataSet& dataSet{expectedDataSet.value()};
-
-  constexpr long double            accelerometerThreshold{1.99L};
-  constexpr long double            gyroscopeThreshold{1999.99L};
-  const std::vector<cl::DataPoint> aboveThresholdDataPoints{
-    ctg::aboveThreshold(dataSet, accelerometerThreshold, gyroscopeThreshold)};
-
-  fmt::print(
-    "{} data points above / below threshold were found.\n",
-    aboveThresholdDataPoints.size());
-
-  for (const cl::DataPoint& dataPoint : aboveThresholdDataPoints) {
-    fmt::print("{}\n", dataPoint);
+    for (const cl::DataPoint& dataPoint : aboveThresholdDataPoints) {
+      fmt::print("{}\n", dataPoint);
+    }
   }
 
   return EXIT_SUCCESS;
