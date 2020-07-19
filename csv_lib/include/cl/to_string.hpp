0b0b41a211d374f25de63ba2b3b2795e8af4230c
CppPhil <CppPhil@users.noreply.github.com>
2020-07-19 22:31:10 +0200

    Update

--- csv_lib/include/cl/to_string.hpp
+++ csv_lib/include/cl/to_string.hpp
@@ -2,14 +2,34 @@
 #define INCG_CL_TO_STRING_HPP
 #include <sstream>
 #include <string>
+#include <type_traits>
+
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+
+#include <pl/meta/remove_cvref.hpp>
 
 namespace cl {
 template<typename Ty>
 inline auto to_string(const Ty& ty)
 {
-  std::ostringstream oss{};
-  oss << ty;
-  return oss.str();
+  using Type = pl::meta::remove_cvref_t<Ty>;
+
+  if constexpr (
+    std::is_same_v<Type, int>
+    || std::is_same_v<Type, long>
+    || std::is_same_v<Type, long long>
+    || std::is_same_v<Type, unsigned>
+    || std::is_same_v<Type, unsigned long>
+    || std::is_same_v<Type, unsigned long long>
+    ) {
+    fmt::format_int formatter{};
+    return formatter.str();
+  } else {
+    std::ostringstream oss{};
+    oss << ty;
+    return oss.str();
+  }
 }
 } // namespace cl
 #endif // INCG_CL_TO_STRING_HPP
