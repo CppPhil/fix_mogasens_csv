52ec83a3ce090c5478cd0a6419d999c9c8c1a409
CppPhil <CppPhil@users.noreply.github.com>
2020-07-25 21:39:13 +0200

    Basic filter python script

--- python/modules/sensor_to_string.py
+++ python/modules/sensor_to_string.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
 
+from .sensors import *
+
 
 def sensor_to_string(sensor_id):
-  left_arm_sensor_id = 769
-  belly_sensor_id = 770
-  right_arm_sensor_id = 771
-  chest_sensor_id = 772
-
-  if sensor_id == left_arm_sensor_id:
+  if sensor_id == left_arm_sensor_id():
     return "left arm sensor"
-  elif sensor_id == belly_sensor_id:
+  elif sensor_id == belly_sensor_id():
     return "belly sensor"
-  elif sensor_id == right_arm_sensor_id:
+  elif sensor_id == right_arm_sensor_id():
     return "right arm sensor"
-  elif sensor_id == chest_sensor_id:
+  elif sensor_id == chest_sensor_id():
     return "chest sensor"
   else:
     return f"bogus sensor, id: {sensor_id}"
---
+++ python/modules/sensors.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+
+def left_arm_sensor_id():
+    return 769
+
+
+def belly_sensor_id():
+    return 770
+
+
+def right_arm_sensor_id():
+    return 771
+
+
+def chest_sensor_id():
+    return 772
+
+
+def sensors():
+    return [left_arm_sensor_id(), belly_sensor_id(), right_arm_sensor_id(), chest_sensor_id()]
