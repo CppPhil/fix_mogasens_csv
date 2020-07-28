82d8f54a417f57903b49459c0f3eb84f1171d436
CppPhil <CppPhil@users.noreply.github.com>
2020-07-28 23:42:51 +0200

    Adjust segment.rb command line API style

--- ruby/segment.rb
+++ ruby/segment.rb
@@ -5,24 +5,26 @@ require_relative 'modules/generate_images_module'
 options = {}
 
 OptionParser.new do |opt|
-  opt.on('--csv_file_path', 'Path to the CSV file to segment.') do |o|
+  opt.on('--csv_file_path=PATH', 'Path to the CSV file to segment.') do |o|
     options[:csv_file_path] = o
   end
 
-  opt.on('--sensor', 'The sensor to use (769 | 770 | 771 | 772)') do |o|
+  opt.on('--sensor=SENSOR', 'The sensor to use (769 | 770 | 771 | 772)') do |o|
     options[:sensor] = o
   end
 
-  opt.on('--channel', 'The channel to use (1 | 2 | 3 | 4 | 5 | 6)') do |o|
+  opt.on('--channel=CHANNEL',\
+         'The channel to use (1 | 2 | 3 | 4 | 5 | 6)') do |o|
     options[:channel] = o
   end
 
-  opt.on('--segmentation_kind',\
+  opt.on('--segmentation_kind=KIND',\
          'The segmentation kind to use (min | max | both)') do |o|
     options[:segmentation_kind] = o
   end
 
-  opt.on('--out_dir', 'Path to the directory to write the segments to.') do |o|
+  opt.on('--out_dir=DIR',\
+         'Path to the directory to write the segments to.') do |o|
     options[:out_dir] = o
   end
 
