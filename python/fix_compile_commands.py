bc9e20fe2b3e73c75ceae5c892d23e0283e52ced
CppPhil <CppPhil@users.noreply.github.com>
2020-07-31 17:27:50 +0200

    fixes for Dlib

--- python/fix_compile_commands.py
+++ python/fix_compile_commands.py
@@ -9,7 +9,8 @@ with open('build/compile_commands.json') as json_file:
   data = json.load(json_file)
   for o in data:
     if not "external" in o['file']:
-      output_data.append(o)
+      if not "_deps" in o['file']:
+        output_data.append(o)
 
 with open('build/compile_commands.json', 'w') as outfile:
   json.dump(output_data, outfile)
