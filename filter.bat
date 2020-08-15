c21c9bcc6eb877ef7424396b7bc98094e35a9a60
CppPhil <CppPhil@users.noreply.github.com>
2020-08-15 15:27:41 +0200

    Try to fix directories with spaces not working on Windows

--- filter.bat
+++ filter.bat
@@ -4,14 +4,14 @@ set dir=%~dp0
 set prev_dir="%CD%"
 set goto_ruby="%dir%\external\goto-ruby\goto-ruby.bat"
 
-cd %dir%
+cd "%dir%"
 
-call %goto_ruby% "%dir%\ruby\filter.rb" %*
+call "%goto_ruby%" "%dir%\ruby\filter.rb" %*
 if errorlevel 1 (
-  cd %prev_dir%
+  cd "%prev_dir%"
   exit /B 1
 )
 
-cd %prev_dir%
+cd "%prev_dir%"
 exit /B 0
 
