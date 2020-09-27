22659d5e48483e973e57951bc17361233d7e61fa
CppPhil <CppPhil@users.noreply.github.com>
2020-09-27 03:44:17 +0200

    Python: Segmentation: Skip over the window if a local extrema has been found.

--- python/modules/segmentation_points.py
+++ python/modules/segmentation_points.py
@@ -25,8 +25,9 @@ def find_local_extrema(data, segmentation_kind, radius):
     if ((segmentation_kind & SegmentationKind.LOCAL_MINIMA) and all(current_value < element for element in window)) \
        or ((segmentation_kind & SegmentationKind.LOCAL_MAXIMA) and all(current_value > element for element in window)):
       extrema.append(current_index)
-
-    current_index += 1
+      current_index = window_end + 1  # Skip over the window after having found a value.
+    else:
+      current_index += 1
 
   return extrema
 
