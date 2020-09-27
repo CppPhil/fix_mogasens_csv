ecbe556720be5b064442b4bc40222664e0675479
CppPhil <CppPhil@users.noreply.github.com>
2020-09-27 03:32:42 +0200

    Python: find indices of local extrema manually so that the windows don't overlap.

--- python/modules/segmentation_points.py
+++ python/modules/segmentation_points.py
@@ -1,23 +1,36 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
-import numpy as np
-from scipy.signal import argrelextrema
-
 from .segmentation_kind import SegmentationKind
 
 
+def find_local_extrema(data, segmentation_kind, radius):
+  extrema = []
+  current_index = 0
+
+  while current_index < len(data):
+    # Closed range
+    window_begin = current_index - radius
+    window_end = current_index + radius
+
+    # Clamp
+    if window_begin < 0:
+      window_begin = 0
+    if window_end >= len(data):
+      window_end = len(data) - 1
+
+    window = data[window_begin:current_index] + data[current_index +
+                                                     1:window_end + 1]
+    current_value = data[current_index]
+
+    if ((segmentation_kind & SegmentationKind.LOCAL_MINIMA) and all(current_value < element for element in window)) \
+       or ((segmentation_kind & SegmentationKind.LOCAL_MAXIMA) and all(current_value > element for element in window)):
+      extrema.append(current_index)
+
+    current_index += 1
+
+  return extrema
+
+
 def segmentation_points(normed_data, segmentation_kind, window_size):
-  data = np.array(normed_data)
   radius = int((window_size - 1) / 2)
-  result = []
-
-  # TODO: Fix overlapping of these windows.
-  if segmentation_kind & SegmentationKind.LOCAL_MINIMA:
-    result.extend(argrelextrema(data, np.less, order=radius)[0].tolist())
-
-  if segmentation_kind & SegmentationKind.LOCAL_MAXIMA:
-    result.extend(argrelextrema(data, np.greater, order=radius)[0].tolist())
-
-  result.sort()
-
-  return result
+  return find_local_extrema(normed_data, segmentation_kind, radius)
