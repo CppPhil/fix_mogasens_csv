8935634c9f456977764215d02971063579dc80ca
CppPhil <CppPhil@users.noreply.github.com>
2020-09-28 21:46:56 +0200

    Add platform specific scripts.
    Add platfrom specific scripts to invoke preprocessed_segment.rb.
    Update TODO_LIST.txt.

---
+++ preprocessed_segment.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set dir=%~dp0
+set prev_dir=%CD%
+set goto_ruby=%dir%\external\goto-ruby\goto-ruby.bat
+
+cd "%dir%"
+
+call "%goto_ruby%" "%dir%\ruby\preprocessed_segment.rb" %*
+if errorlevel 1 (
+  cd %prev_dir%
+  exit /B 1
+)
+
+cd "%prev_dir%"
+exit /B 0
+
