6a5c9fe7d8cd5978228d48863ec9a4241f073628
CppPhil <CppPhil@users.noreply.github.com>
2020-11-04 09:26:19 +0100

    Document logFiles

--- compare_segmentation/include/log_files.hpp
+++ compare_segmentation/include/log_files.hpp
@@ -8,6 +8,11 @@
 #include <cl/fs/path.hpp>
 
 namespace cs {
+/*!
+ * \brief Fetches the paths to the log files in the given directory.
+ * \param directoryPath The path to a directory to search for log files.
+ * \return The log files found or an error.
+ **/
 [[nodiscard]] cl::Expected<std::vector<cl::fs::Path>> logFiles(
   pl::string_view directoryPath);
 } // namespace cs
--- compare_segmentation/src/log_files.cpp
+++ compare_segmentation/src/log_files.cpp
@@ -17,11 +17,14 @@ cl::Expected<std::vector<cl::fs::Path>> logFiles(pl::string_view directoryPath)
 
   std::vector<cl::fs::Path> listing{*std::move(expectedListing)};
 
+  // Disregard files that aren't logs.
   pl::algo::erase_if(listing, [](const cl::fs::Path& currentPath) {
     const pl::string_view stringView{currentPath.str()};
     return !stringView.ends_with(".log");
   });
 
+  // Prepend the path to the directory to the file paths, because
+  // directoryListing doesn't include them.
   pl::algo::transform(
     listing, listing.begin(), [directoryPath](const cl::fs::Path& path) {
       return cl::fs::Path{directoryPath.to_string() + "/" + path.str()};
