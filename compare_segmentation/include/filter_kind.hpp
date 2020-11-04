73becfbd3baf44f3a400b214f457059edb9804a9
CppPhil <CppPhil@users.noreply.github.com>
2020-11-04 09:23:01 +0100

    Document FilterKind

--- compare_segmentation/include/filter_kind.hpp
+++ compare_segmentation/include/filter_kind.hpp
@@ -3,8 +3,17 @@
 #include <iosfwd>
 
 namespace cs {
+/*!
+ * \brief Type for the different kinds of filters.
+ **/
 enum class FilterKind { Butterworth, MovingAverage };
 
+/*!
+ * \brief Prints a FilterKind to an ostream.
+ * \param os The ostream to print to.
+ * \param filterKind The FilterKind to print.
+ * \return `os`
+ **/
 std::ostream& operator<<(std::ostream& os, FilterKind filterKind);
 } // namespace cs
 #endif // INCG_CS_FILTER_KIND_HPP
