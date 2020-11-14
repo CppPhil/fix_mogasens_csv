154d33426781962ef658d23b372a65e3a9834086
CppPhil <CppPhil@users.noreply.github.com>
2020-11-14 22:08:59 +0100

    Handle exceptions

--- confusion_matrix/src/main.cpp
+++ confusion_matrix/src/main.cpp
@@ -8,21 +8,32 @@
 
 int main()
 {
-  // TODO: HERE
-  // TODO: This is just experimental.
-  const std::unordered_map<
-    cm::DataSetIdentifier,
-    std::vector<cm::ManualSegmentationPoint>>
-    manualSegmentationPointsMap{cm::ManualSegmentationPoint::readCsvFile()};
+  try { // TODO: HERE
+    // TODO: This is just experimental.
+    const std::unordered_map<
+      cm::DataSetIdentifier,
+      std::vector<cm::ManualSegmentationPoint>>
+      manualSegmentationPointsMap{cm::ManualSegmentationPoint::readCsvFile()};
 
-  const std::unordered_map<cl::fs::Path, std::vector<std::uint64_t>> res
-    = cm::segment();
+    const std::unordered_map<cl::fs::Path, std::vector<std::uint64_t>> res
+      = cm::segment();
 
-  (void)manualSegmentationPointsMap;
-  (void)res;
+    (void)manualSegmentationPointsMap;
+    (void)res;
 
-  for (const auto& [path, segmentationPoints] : res) {
-    fmt::print("\"{}\": \"{}\"\n", path, fmt::join(segmentationPoints, ", "));
+    for (const auto& [path, segmentationPoints] : res) {
+      fmt::print("\"{}\": \"{}\"\n", path, fmt::join(segmentationPoints, ", "));
+    }
+  }
+  catch (const cl::Exception& ex) {
+    fmt::print(stderr, "{}: caught cl::Exception\n", PL_CURRENT_FUNCTION);
+    fmt::print(
+      stderr,
+      "Thrown at file: \"{}\", line: {}, function: \"{}\"\n",
+      ex.file(),
+      ex.line(),
+      ex.function());
+    fmt::print(stderr, "Message: \"{}\"\n", ex.what());
   }
 
   fmt::print("DONE.\n");
