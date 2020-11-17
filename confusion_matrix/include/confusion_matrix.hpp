754809552f8a3d1bdac68cc2e774eca4852e52fc
CppPhil <CppPhil@users.noreply.github.com>
2020-11-17 21:39:56 +0100

    Add ConfusionMatrix type

---
+++ confusion_matrix/include/confusion_matrix.hpp
@@ -0,0 +1,50 @@
+#ifndef INCG_CM_CONFUSION_MATRIX_HPP
+#define INCG_CM_CONFUSION_MATRIX_HPP
+#include <cstdint>
+
+namespace cm {
+// TODO:
+// TODO: Iterate over all the valid hardware timestamps for each Path thingie
+//       and check if the current timestamp is a true positive, true negative
+//       false positive or false negative
+class ConfusionMatrix {
+public:
+  using this_type = ConfusionMatrix;
+
+  ConfusionMatrix();
+
+  [[nodiscard]] std::uint64_t truePositives() const noexcept;
+
+  [[nodiscard]] std::uint64_t trueNegatives() const noexcept;
+
+  [[nodiscard]] std::uint64_t falsePositives() const noexcept;
+
+  [[nodiscard]] std::uint64_t falseNegatives() const noexcept;
+
+  [[nodiscard]] std::uint64_t totalCount() const noexcept;
+
+  this_type& incrementTruePositives() noexcept;
+
+  this_type& incrementTrueNegatives() noexcept;
+
+  this_type& incrementFalsePositives() noexcept;
+
+  this_type& incrementFalseNegatives() noexcept;
+
+private:
+  // If a given fake one exists within the range of a real one.
+  std::uint64_t m_truePositives;
+
+  // If there's no real one and no fake one for that as well.
+  std::uint64_t m_trueNegatives;
+
+  // If a given fake one is not within the range of any real one.
+  std::uint64_t m_falsePositives;
+
+  // If for a given real one there is no fake one to be found in its range.
+  std::uint64_t m_falseNegatives;
+
+  std::uint64_t m_totalCount;
+};
+} // namespace cm
+#endif // INCG_CM_CONFUSION_MATRIX_HPP
---
+++ confusion_matrix/src/confusion_matrix.cpp
@@ -0,0 +1,65 @@
+#include "confusion_matrix.hpp"
+
+namespace cm {
+ConfusionMatrix::ConfusionMatrix()
+  : m_truePositives{0}
+  , m_trueNegatives{0}
+  , m_falsePositives{0}
+  , m_falseNegatives{0}
+  , m_totalCount{0}
+{
+}
+
+std::uint64_t ConfusionMatrix::truePositives() const noexcept
+{
+  return m_truePositives;
+}
+
+std::uint64_t ConfusionMatrix::trueNegatives() const noexcept
+{
+  return m_trueNegatives;
+}
+
+std::uint64_t ConfusionMatrix::falsePositives() const noexcept
+{
+  return m_falsePositives;
+}
+
+std::uint64_t ConfusionMatrix::falseNegatives() const noexcept
+{
+  return m_falseNegatives;
+}
+
+std::uint64_t ConfusionMatrix::totalCount() const noexcept
+{
+  return m_totalCount;
+}
+
+ConfusionMatrix& ConfusionMatrix::incrementTruePositives() noexcept
+{
+  ++m_truePositives;
+  ++m_totalCount;
+  return *this;
+}
+
+ConfusionMatrix& ConfusionMatrix::incrementTrueNegatives() noexcept
+{
+  ++m_trueNegatives;
+  ++m_totalCount;
+  return *this;
+}
+
+ConfusionMatrix& ConfusionMatrix::incrementFalsePositives() noexcept
+{
+  ++m_falsePositives;
+  ++m_totalCount;
+  return *this;
+}
+
+ConfusionMatrix& ConfusionMatrix::incrementFalseNegatives() noexcept
+{
+  ++m_falseNegatives;
+  ++m_totalCount;
+  return *this;
+}
+} // namespace cm
