5a1c01c2b9b8f0e789910d3a6c377f99b0efb351
CppPhil <CppPhil@users.noreply.github.com>
2020-11-19 15:41:35 +0100

    Fix do_it.sh

--- do_it.sh
+++ do_it.sh
@@ -3,11 +3,15 @@
 ARRAY=()
 
 for file in ./resources/preprocessed/Interpolated-Revised/*.csv; do
-  ARRAY+=($file)
+  if [[ $file =~ "_with_repetition_ids" ]]; then
+    continue
+  else
+    ARRAY+=($file)
+  fi
 done
 
 for e in "${ARRAY[@]}"; do
-  ./preprocessed_segment.sh --skip_window=false --delete_too_close=false --delete_low_variance=false --image_format=png --csv_file_path=$file --imu=accelerometer --segmentation_kind=max --window_size=501 --filter=butterworth
+  ./preprocessed_segment.sh --skip_window=false --delete_too_close=false --delete_low_variance=false --image_format=png --csv_file_path=$e --imu=accelerometer --segmentation_kind=max --window_size=$1 --filter=butterworth
 done
 
 exit 0
