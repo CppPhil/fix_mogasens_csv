6b644cf4594e9fa72434304dec3c6150d767bde4
CppPhil <CppPhil@users.noreply.github.com>
2020-11-24 22:01:52 +0100

    Fix documentation

--- csv_lib/include/cl/fs/windows.hpp
+++ csv_lib/include/cl/fs/windows.hpp
@@ -27,7 +27,7 @@ namespace cl::fs {
 [[nodiscard]] std::wstring utf8ToUtf16(pl::string_view utf8);
 
 /*!
- * \brief Converts a UTF-16 encoded wide character string to UTF-6 string.
+ * \brief Converts a UTF-16 encoded wide character string to UTF-8 string.
  * \param utf16 The UTF-16 encoded wide character string to convert.
  * \return The resulting UTF-8 string.
  * \note Most WINAPIs expect UTF-16 encoded wide character strings, but
